3880f76be376fb865ffe376ebbeb85f08051d1ab
Ignatius Filbert Sevilen <81659972+FilbertSevilen1@users.noreply.github.com>
2025-05-17 13:53:18 +0700

    Bug Fix for Docker

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,6 @@
+require("dotenv").config(); // Load environment variables from .env
 const mongoose = require("mongoose");
-module.exports = mongoose.connect("mongodb+srv://user1:zbCQ7MhNS2r5Uiw7@birthdayservice.gfe3aok.mongodb.net/birthday_service?retryWrites=true&w=majority&appName=BirthdayService");
+
+const mongoUri = process.env.MONGODB_URI;
+
+module.exports = mongoose.connect(mongoUri);
